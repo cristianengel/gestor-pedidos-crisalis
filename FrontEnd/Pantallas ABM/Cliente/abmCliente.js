f2b1aaeb106977f49e61f3e242c00ff189900cc2
cristianengel <cristianengel1411@gmail.com>
2022-12-24 14:20:21 -0300

    Vinculación de una pantalla

--- FrontEnd/Pantallas ABM/Cliente/abmCliente.js
+++ FrontEnd/Pantallas ABM/Cliente/abmCliente.js
@@ -1,5 +1,11 @@
 const homeButton = document.querySelector("#home-btn");
+const addBtn = document.querySelector("#add-btn");
 
 homeButton.addEventListener("click", () => {
     open("../../Homepage/homepage.html", "_self");
 })
+
+addBtn.addEventListener("click", () => {
+    open("../../Alta/Cliente/alta-cliente.html", "_self")
+})
+

a9cad92d6ec56035da14b74a069e992fd6ad2ecd
cristianengel <cristianengel1411@gmail.com>
2023-01-20 12:14:31 -0300

    Avances en Pedidos

--- FrontEnd/Pedido/pedido.js
+++ FrontEnd/Pedido/pedido.js
@@ -32,6 +32,7 @@ const clientTable = document.querySelector("#client-table");
 const clientTableBody = document.querySelector("#client-tbody");
 const clientTableHead = document.querySelector("#client-thead");
 const clientBottomBar = document.querySelector("#client-bottom-bar");
+const orderBottomBar = document.querySelector("#order-bottom-bar");
 const detailTable = document.querySelector("#detail-table");
 const detailTableBody = document.querySelector("#detail-tbody");
 const detailTableHead = document.querySelector("#detail-thead");
@@ -42,20 +43,38 @@ const productBtn = document.querySelector("#product-btn");
 const serviceBtn = document.querySelector("#service-btn");
 const backBtnAssetType = document.querySelector("#back-btn-asset-type");
 const productListDiv = document.querySelector(".product-list-div");
+const serviceListDiv = document.querySelector(".service-list-div");
 const productTable = document.querySelector("#product-table");
 const productTableHead = document.querySelector("#product-thead");
 const productTableBody = document.querySelector("#product-tbody");
+const serviceTable = document.querySelector("#service-table");
+const serviceTableHead = document.querySelector("#service-thead");
+const serviceTableBody = document.querySelector("#service-tbody");
 const listInput = document.querySelector("#list-input");
 const searchBtn = document.querySelector("#search-btn");
+const quantityInput = document.querySelector("#quantity-input");
+const confirmOrderBtn = document.querySelector("#confirm-order-btn");
 let newClientData = {};
 let isClientSelected = false;
 
 const clientListLink = "http://localhost:8080/client/list";
 const addClientLink = "http://localhost:8080/client/new";
 const productListLink = "http://localhost:8080/asset/products";
+const serviceListLink = "http://localhost:8080/asset/services";
 
 async function search() {
     refreshTable("./headers.json", `http://localhost:8080/client/get_by_identification?identification=${listInput.value}`)
+}
+
+function getToday() {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+
+    today = yyyy + "-" + mm + "-" + dd
+
+    return today;
 }
 
 function cleanInputs() {
@@ -63,6 +82,7 @@ function cleanInputs() {
     isClientSelected = false;
     clientBottomBar.value = "";
     listInput.value = "";
+    quantityInput.value = 1;
 }
 
 async function fetchDataFromDB(url) {
@@ -140,6 +160,90 @@ async function newClient(isBusiness) {
     refreshTable("./headers.json", clientListLink)
 }
 
+async function newOrder() {
+    let detailList = [];
+    let responseData;
+    
+    for (let i = 1, row; row = detailTable.rows[i]; i++) {
+
+        let data = {
+            priceSell: parseFloat(row.cells[5].innerHTML),
+            quantity: parseInt(row.cells[4].innerHTML)
+        }
+        console.log(data)
+        const response = await fetch(`http://localhost:8080/order_detail/new?assetId=${parseInt(row.cells[0].innerHTML)}`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(res => res.json())
+        .then(data => responseData = data);
+        
+        let responseDataArray = Object.entries(responseData);
+        detailList.push(responseDataArray[0][1]);
+        console.log(responseDataArray[0][1])
+        console.log(responseDataArray)
+    }
+    console.log(detailList);
+
+    let data = {
+        date: getToday(),
+        total: orderBottomBar.value
+    }
+
+    const secondResponse = await fetch(`http://localhost:8080/order/new?orderDetailsId=${detailList}&clientId=${clientBottomBar.value}`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json())
+    .then(data => responseData = data);
+    console.log(responseData.id)
+}
+
+function loadDetailIntoTable(detail) {
+    const rowElement = document.createElement("tr");
+
+    let cellElement = document.createElement("td")
+    cellElement.textContent = detail.id
+    rowElement.appendChild(cellElement)
+
+    let cellElement2 = document.createElement("td")
+    cellElement2.textContent = detail.name
+    rowElement.appendChild(cellElement2)
+
+    let cellElement3 = document.createElement("td")
+    cellElement3.textContent = detail.basePrice
+    rowElement.appendChild(cellElement3)
+
+    let cellElement4 = document.createElement("td")
+    cellElement4.textContent = detail.extraCharges
+    rowElement.appendChild(cellElement4)
+
+    let cellElement5 = document.createElement("td")
+    cellElement5.textContent = detail.quantity
+    rowElement.appendChild(cellElement5)
+
+    let cellElement6 = document.createElement("td")
+    cellElement6.textContent = detail.totalPrice
+    rowElement.appendChild(cellElement6)
+
+    detailTableBody.appendChild(rowElement);
+
+    for (let i = 1, row; row = detailTable.rows[i]; i++) {
+        for (let j = 0, col; col = row.cells[j]; j++) {
+            if(col.innerHTML == "") {
+                col.innerHTML = "-";
+            }
+        }
+    }
+
+    refreshTotal();
+}
+
 function loadBody(data) {
     for (let dataObject of data) {
         const rowElement = document.createElement("tr");
@@ -175,7 +279,7 @@ function loadBody(data) {
     }
 }
 
-function loadAssetsBody(data) {
+function loadProductsBody(data) {
     for (let dataObject of data) {
         const rowElement = document.createElement("tr");
         let dataObjectArray = Object.entries(dataObject);
@@ -199,8 +303,98 @@ function loadAssetsBody(data) {
     }
     for (let i = 1, row; row = productTable.rows[i]; i++) {
         //iterate through rows
-        row.addEventListener("click", () => {
-            //TODO
+        row.addEventListener("click", async () => {
+            let basePrice = parseInt(row.cells[2].innerHTML);
+            let taxes = row.cells[3].innerHTML.split(",");
+            let price = basePrice;
+            let responseData;
+            for(tax of taxes) {
+                const response = await fetch(`http://localhost:8080/tax/get_by_name?name=${tax}`)
+                    .then(res => res.json())
+                    .then(data => responseData = data);
+            
+                let responseDataArray = Object.entries(responseData);
+
+                let charge = price * (responseDataArray[2][1] / 100);
+                price = price + charge;
+            }
+            let detail = {
+                id: row.cells[0].innerHTML,
+                name: row.cells[1].innerHTML,
+                basePrice: price,
+                extraCharges: null,
+                quantity: quantityInput.value,
+                totalPrice: parseFloat((price * quantityInput.value).toFixed(2))
+            }
+            loadDetailIntoTable(detail);
+            productListDiv.style.display = "none";
+            assetFormDiv.style.display = "none";
+        })
+        if(i % 2 == 0 && i > 0) {
+            row.style.backgroundColor = "#EEEEEE"
+        }
+        //rows would be accessed using the "row" variable assigned in the for loop
+        for (let j = 0, col; col = row.cells[j]; j++) {
+          //iterate through columns
+          //columns would be accessed using the "col" variable assigned in the for loop
+          if (col.innerHTML == "") {
+            col.innerHTML = "-";
+          }
+        }  
+    }
+}
+
+function loadServicesBody(data) {
+    for (let dataObject of data) {
+        console.log(dataObject)
+        const rowElement = document.createElement("tr");
+        let dataObjectArray = Object.entries(dataObject);
+        for(let i of dataObjectArray) {
+            if(dataObjectArray.indexOf(i) == 3) continue;
+            let cellElement = document.createElement("td");
+            if(dataObjectArray.indexOf(i) == 5) {
+                let taxesArray = []
+                for(let j of dataObjectArray[dataObjectArray.indexOf(i)][1]) {
+                    let dataTaxesArray = Object.entries(j)
+                    taxesArray.push(dataTaxesArray[1][1])
+                }
+                cellElement.textContent = taxesArray;
+                rowElement.appendChild(cellElement);
+                continue;
+            }
+            cellElement.textContent = i[1];
+            rowElement.appendChild(cellElement);
+        }
+        serviceTableBody.appendChild(rowElement)
+    }
+    for (let i = 1, row; row = serviceTable.rows[i]; i++) {
+        row.addEventListener("click", async () => {
+            let basePrice = parseInt(row.cells[2].innerHTML);
+            let taxes = row.cells[4].innerHTML.split(",");
+            let extraCharges = row.cells[3].innerHTML;
+            let price = basePrice;
+            let responseData;
+            for(tax of taxes) {
+                const response = await fetch(`http://localhost:8080/tax/get_by_name?name=${tax}`)
+                    .then(res => res.json())
+                    .then(data => responseData = data);
+            
+                let responseDataArray = Object.entries(responseData);
+
+                let charge = price * (responseDataArray[2][1] / 100);
+                price = price + charge;
+            }
+            let detail = {
+                id: row.cells[0].innerHTML,
+                name: row.cells[1].innerHTML,
+                basePrice: parseFloat(price.toFixed(2)),
+                extraCharges: row.cells[3].innerHTML,
+                quantity: 1,
+                totalPrice: parseFloat(price.toFixed(2)) + parseFloat(extraCharges)
+            }
+            loadDetailIntoTable(detail);
+            serviceListDiv.style.display = "none";
+            assetFormDiv.style.display = "none";
         })
         if(i % 2 == 0 && i > 0) {
             row.style.backgroundColor = "#EEEEEE"
@@ -239,7 +433,7 @@ async function refreshTable(urlHeaders, urlBody) {
     });
 }
 
-async function refreshAssetsTable(urlHeaders, urlBody) {
+async function refreshProductsTable(urlHeaders, urlBody) {
     // Headers
     const headersResponse = await fetch(urlHeaders);
     const { headers } = await headersResponse.json();
@@ -258,8 +452,40 @@ async function refreshAssetsTable(urlHeaders, urlBody) {
     // Body
     productTableBody.innerHTML = "";
     fetchDataFromDB(urlBody).then(data => {
-        loadAssetsBody(data);
+        loadProductsBody(data);
     });
+}
+
+async function refreshServicesTable(urlHeaders, urlBody) {
+    // Headers
+    const headersResponse = await fetch(urlHeaders);
+    const { headers } = await headersResponse.json();
+
+    // Clear the headers
+    serviceTableHead.innerHTML = "<tr></tr>";
+
+    // Populate Headers
+    for (const headerText of headers) {
+        const headerElement = document.createElement("th");
+
+        headerElement.textContent = headerText;
+        serviceTableHead.querySelector("tr").appendChild(headerElement);
+    }
+
+    // Body
+    serviceTableBody.innerHTML = "";
+    fetchDataFromDB(urlBody).then(data => {
+        loadServicesBody(data);
+    });
+}
+
+function refreshTotal() {
+    let total = 0.0;
+    for (let i = 1, row; row = detailTable.rows[i]; i++) {
+        total = total + parseFloat(row.cells[5].innerHTML);
+        total = parseFloat(total.toFixed(2));
+    }
+    orderBottomBar.value = total;
 }
 
 refreshTable("./headers.json", clientListLink)
@@ -355,9 +581,17 @@ backBtnAssetType.addEventListener("click", () => {
 productBtn.addEventListener("click", () => {
     assetTypeDiv.style.display = "none";
     productListDiv.style.display = "inline";
-    refreshAssetsTable("./product-headers.json", productListLink)
+    refreshProductsTable("./product-headers.json", productListLink)
+    quantityInput.value = 1;
 })
 
 serviceBtn.addEventListener("click", () => {
+    assetTypeDiv.style.display = "none";
+    serviceListDiv.style.display = "inline";
+    refreshServicesTable("./service-headers.json", serviceListLink)
+})
 
+confirmOrderBtn.addEventListener("click", () => {
+    if(clientBottomBar.value == "") return;
+    newOrder();
 })
